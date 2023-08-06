b38813cb3bb61d1c52c92c5966032e745ebb7cb7
Marlon GARCIA <clasesucatmarlon@gmail.com>
2023-08-06 18:21:16 -0500

    feat: :art: Created structure project
    Created structure project

---
+++ Activities/Taller1SoftlondPOO/src/main/java/com/magmcode/Taller1SoftlondPOO/Taller1SoftlondPooApplication.java
@@ -0,0 +1,13 @@
+package com.magmcode.Taller1SoftlondPOO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Taller1SoftlondPooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Taller1SoftlondPooApplication.class, args);
+	}
+
+}
---
+++ Activities/Taller1SoftlondPOO/src/test/java/com/magmcode/Taller1SoftlondPOO/Taller1SoftlondPooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.magmcode.Taller1SoftlondPOO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Taller1SoftlondPooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
